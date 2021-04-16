174b6651ab4a28bc32c2a820ad5cf7ca18df1bb5
james white jr <jameswhitejr.@jamess-MBP-2.home>
2021-04-15 20:15:07 -0400

    added sdk

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1618530692.3807685
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://imgur.com/a/7Xx0m6Q',
+  defaultTitle: 'Trek Notify',
+  defaultActionURL: 'https://treknotify.com/',
+  appKey: 'zNNG1hT7SsyA9Og15sA8kA',
+  token: 'MTp6Tk5HMWhUN1NzeUE5T2cxNXNBOGtBOlVCUzROMFZiaFVPOGdlODJtNHVGZHI2TEFNc3Y1X05hVTNJT3FUWVdrMHc',
+  vapidPublicKey: 'BIrIv6yIqEolM5XKDhOFVXqdcw4BGmENuGXJ5LGTVc8k8sPmivYg7l_r5HTejLpRDiuYBp0dzk7uwLL7Kaw4wJk='
+})
