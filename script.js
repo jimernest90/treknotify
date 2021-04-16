0515b4074d7d18af06c620f717487313bdbc6b54
james white jr <jameswhitejr.@jamess-MBP-2.home>
2021-04-16 18:52:53 -0400

    added dark mode

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let healthTwo = document.querySelector('.health.health-two')
 let healthThree = document.querySelector('.health.health-three')
 let monitor = document.querySelector('.message-box')
+let slider = document.querySelector('.slider')
+let leftBox = document.querySelector('.left-box')
+let roundedBox = document.querySelector('.rounded-box')
+let checkedSlider = document.querySelector('.input:checked')
+let fullScreen = document.querySelector('.full-screen')
 
 healthTwo.onclick = function(){
     monitor.style.background = `url('./img/monitor-view-one.jpeg')`
@@ -15,3 +20,8 @@ healthThree.onclick = function(){
     
 }
 
+slider.addEventListener("click", function(){
+    leftBox.classList.toggle('dark-mode-boxes'); 
+    roundedBox.classList.toggle('dark-mode-boxes');
+    fullScreen.classList.toggle('dark-mode-background')
+  });
