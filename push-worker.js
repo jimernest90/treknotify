4eceed3495d936938160ec1d273c759f863634fe
james white jr <jameswhitejr.@jamess-MBP-2.home>
2021-04-16 10:32:23 -0400

    trouble shooting sdk prompt

--- push-worker.js
+++ push-worker.js
@@ -1,7 +1,7 @@
 // 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1618530692.3807685
 importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
 uaSetup.worker(self, {
-  defaultIcon: 'https://imgur.com/a/7Xx0m6Q',
+  defaultIcon: 'https://i.imgur.com/ZXEvmUb.png',
   defaultTitle: 'Trek Notify',
   defaultActionURL: 'https://treknotify.com/',
   appKey: 'zNNG1hT7SsyA9Og15sA8kA',
