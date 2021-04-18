08b24c40373668b648b88fbf659b9900f1fa0522
james white jr <jameswhitejr.@jamess-MBP-2.home>
2021-04-18 14:49:44 -0400

    final design changes

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 let healthTwo = document.querySelector('.health.health-two')
 let healthThree = document.querySelector('.health.health-three')
+let healthFour = document.querySelector('.health.health-four')
 let monitor = document.querySelector('.message-box')
 let slider = document.querySelector('.slider')
-let leftBox = document.querySelector('.left-box')
-let roundedBox = document.querySelector('.rounded-box')
 let checkedSlider = document.querySelector('.input:checked')
 let fullScreen = document.querySelector('.full-screen')
+let healthOne = document.querySelector('.health-one')
+let features = document.querySelector('.features')
+let health = document.querySelector('.health')
 
 healthTwo.onclick = function(){
     monitor.style.background = `url('./img/monitor-view-one.jpeg')`
     monitor.style.backgroundSize = 'contain'
-    // monitor.style.height = '5em'
 };
 healthThree.onclick = function(){
     monitor.style.background = `url('./img/diagram-two.jpeg')`
     monitor.style.backgroundSize = 'contain'
     monitor.style.backgroundRepeat = 'no-repeat'
-    // monitor.style.height = '20em'
     
 }
+healthOne.addEventListener("click", function(){
+    healthOne.classList.toggle('green')
+})
 
 slider.addEventListener("click", function(){
-    leftBox.classList.toggle('dark-mode-boxes'); 
-    roundedBox.classList.toggle('dark-mode-boxes');
     fullScreen.classList.toggle('dark-mode-background')
+    features.classList.toggle('dark-mode-features')
+    healthOne.classList.toggle('dark-health-one')
+    healthTwo.classList.toggle('dark-health')
+    healthThree.classList.toggle('dark-health')
+    healthFour.classList.toggle('dark-health')
   });
