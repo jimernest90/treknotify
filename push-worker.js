977918a3f7335b23187cd4b8a66b19b326bfa609
james white jr <jameswhitejr.@jamess-MBP-2.home>
2021-04-18 00:18:10 -0400

    .

--- push-worker.js
+++ push-worker.js
@@ -1,9 +1,9 @@
-// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1618615156.002828
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1618717697.2111537
 importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
 uaSetup.worker(self, {
   defaultIcon: 'https://i.imgur.com/jzWZhzc.png',
   defaultTitle: 'Trek Notify',
-  defaultActionURL: 'https://treknotify.com/',
+  defaultActionURL: 'https://treknotify.com',
   appKey: 'zNNG1hT7SsyA9Og15sA8kA',
   token: 'MTp6Tk5HMWhUN1NzeUE5T2cxNXNBOGtBOlVCUzROMFZiaFVPOGdlODJtNHVGZHI2TEFNc3Y1X05hVTNJT3FUWVdrMHc',
   vapidPublicKey: 'BIrIv6yIqEolM5XKDhOFVXqdcw4BGmENuGXJ5LGTVc8k8sPmivYg7l_r5HTejLpRDiuYBp0dzk7uwLL7Kaw4wJk='
